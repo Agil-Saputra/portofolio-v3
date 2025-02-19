57fece507cb58714215e77ed4026e0a8743b0b56
Agil-Saputra <ragelyusuf752@gmail.com>
2025-02-20 01:56:26 +0700

    feat: add new education db & add new data for skills and tools

---
+++ src/app/db/education.js
@@ -0,0 +1,23 @@
+export const education = [
+  {
+    major: "Undergraduate Bachelor‘s Degree of Informatics",
+    institution: "Amikom Yogyakarta University",
+    startMonth: "August",
+    startYear: "2023",
+    endMonth: "",
+    endYear: "",
+    isCurrent: true,
+    relatedCourse:
+      "Object Oriented Programming, Data Structures & Algorithm, Human Computer Interaction.",
+  },
+  {
+    major: "Mathematic & Science",
+    institution: "SMA Xaverius 01 Belitang",
+    startMonth: "August",
+    startYear: "2019",
+    endMonth: "April",
+    endYear: "2020",
+    isCurrent: true,
+    relatedCourse: "",
+  },
+];
--- src/app/db/skills.js
+++ src/app/db/skills.js
@@ -1,15 +1,23 @@
 import react from "../../assets/icons/react.svg"
 import flutter from "../../assets/icons/flutter.svg"
-import html from "../../assets/icons/flutter.svg"
+import html from "../../assets/icons/html.svg"
 import css from "../../assets/icons/css.svg"
-// import javascript from "../../assets/icons/javascript.svg"
+import javascript from "../../assets/icons/js.svg"
+import typescript from "../../assets/icons/typescript.svg"
 import next from "../../assets/icons/next.svg"
 // import jquery from "../../assets/icons/jquery.svg"
 import npm from "../../assets/icons/npm.svg"
-// import tailwind from "../../assets/icons/tailwind.svg"
+import tailwind from "../../assets/icons/tailwind.svg"
 import sass from "../../assets/icons/sass.svg"
 import vue from "../../assets/icons/vue.svg"
 import vite from "../../assets/icons/vite.svg"
+import github from "../../assets/icons/github.svg"
+import postman from "../../assets/icons/postman.svg"
+import redux from "../../assets/icons/redux.svg"
+import zeplin from "../../assets/icons/zeplin.svg"
+import vsCode from "../../assets/icons/vscode.svg"
+import materialUi from "../../assets/icons/material ui.svg"
+import laravel from "../../assets/icons/laravel.svg"
 
 export const skills = [
 	'Web Development',
@@ -31,10 +39,18 @@ export const frontEndSkills = [
 		name : "CSS",
 		icon : css
 	},
-	// {
-	// 	name : "Javascript",
-	// 	icon : javascript
-	// },
+	{
+		name : "Javascript",
+		icon : javascript
+	},
+	{
+		name : "Typescript",
+		icon : typescript
+	},
+	{
+		name : "Tailwind CSS",
+		icon : tailwind
+	},
 	{
 		name : "React JS",
 		icon : react
@@ -42,10 +58,6 @@ export const frontEndSkills = [
 	{
 		name : "Next JS",
 		icon : next
-	},
-	{
-		name : "React JS",
-		icon : react
 	},
 	{
 		name : "Vue JS",
@@ -56,6 +68,10 @@ export const frontEndSkills = [
 		icon : flutter
 	},
 	{
+		name : "Laravel",
+		icon : laravel
+	},
+	{
 		name : "Vite",
 		icon : vite
 	},
@@ -64,8 +80,8 @@ export const frontEndSkills = [
 		icon : sass
 	},
 	{
-		name : "Jquery",
-		icon : vite
+		name : "Material UI",
+		icon : materialUi
 	},
 ]
 
@@ -73,5 +89,25 @@ export const tools = [
 	{
 		name : "NPM",
 		icon : npm
-	}
+	},
+	{
+		name : "Redux",
+		icon : redux
+	},
+	{
+		name : "Github",
+		icon : github
+	},
+	{
+		name : "Postman",
+		icon : postman
+	},
+	{
+		name : "Zeplin",
+		icon : zeplin
+	},
+	{
+		name : "Visual Studio Code",
+		icon : vsCode
+	},
 ]
